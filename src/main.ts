041f5185f1912ae06de4bdbec098960f7ef6b006
Ankarmin <davidspr.22@gmail.com>
2026-06-05 16:55:21 -0500

    feat(api): habilitar swagger para endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { getCorsOrigins, isOriginAllowed } from './config/env';
@@ -37,6 +38,21 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('EcoTrack API')
+    .setDescription('Documentacion de endpoints REST de EcoTrack')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, swaggerDocument, {
+    jsonDocumentUrl: 'swagger/json',
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(Number(process.env.PORT ?? 3000));
 }
